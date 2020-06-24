41e4b26850da5fcec4e51cd32012a5d62ae9614b
Atze00 <lory.atze@gmail.com>
2020-06-24 16:12:51 +0200

    Update ModelsRGB.py

--- ModelsRGB.py
+++ ModelsRGB.py
@@ -165,3 +165,47 @@ class MyNet(nn.Module):
         feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
         feats = self.classifier(feats1)
         return feats, feats1, output_t.squeeze(2)
+    
+    
+class MyNetIDT(nn.Module):
+    def __init__(self, supervision, num_classes=61, mem_size=512, loss_supervision="classification"):
+        super(ConvLSTMAttention, self).__init__()
+        self.supervision = supervision
+        self.num_classes = num_classes
+        self.resNet = resnetMod.resnet34(True, True)
+        self.mem_size = mem_size
+        self.weight_softmax = self.resNet.fc.weight
+        self.lstm_cell = MyConvLSTMCell(512, mem_size)
+        self.avgpool = nn.AvgPool2d(7)
+        self.dropout = nn.Dropout(0.7)
+        self.fc = nn.Linear(mem_size, self.num_classes)
+        self.classifier = nn.Sequential(self.dropout, self.fc)
+        if loss_supervision == "regression":
+            self.sup_head = SupervisionHead(512, 100, 7, 7, 1)
+        if loss_supervision == "classification":
+            self.sup_head = SupervisionHead(512, 100, 7, 7, 2)
+
+    def forward(self, x):
+        state = (torch.zeros((x.size(1), self.mem_size, 7, 7)).cuda(),
+                 torch.zeros((x.size(1), self.mem_size, 7, 7)).cuda())
+        superv_x = []
+        for t in range(x.size(0)):
+            logit, feature_conv, feature_convNBN = self.resNet(x[t])
+            bz, nc, h, w = feature_conv.size()
+            feature_conv1 = feature_conv.view(bz, nc, h * w)
+            probs, idxs = logit.sort(1, True)
+            class_idx = idxs[:, 0]
+            cam = torch.bmm(self.weight_softmax[class_idx].unsqueeze(1), feature_conv1)
+            attention_map = F.softmax(cam.squeeze(1), dim=1)
+            attention_map = attention_map.view(attention_map.size(0), 1, 7, 7)
+            attention_feat = feature_convNBN * attention_map.expand_as(feature_conv)
+            state = self.lstm_cell(attention_feat, state)
+            if self.supervision:
+                superv_x.append(self.sup_head(state[1]))
+        if self.supervision:
+            superv_x = torch.stack(superv_x)
+            superv_x = superv_x.reshape(superv_x.shape[0] * superv_x.shape[1], -1, 7, 7)
+
+        feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
+        feats = self.classifier(feats1)
+        return feats, feats1, superv_x
