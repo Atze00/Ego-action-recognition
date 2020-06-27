1e99c8205dc69b7f0d5c950b6567bf663e8e10e6
Atze00 <lory.atze@gmail.com>
2020-06-27 22:03:18 +0200

    Update ModelsRGB.py

--- ModelsRGB.py
+++ ModelsRGB.py
@@ -209,3 +209,55 @@ class MyNetIDT(nn.Module):
         feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
         feats = self.classifier(feats1)
         return feats, feats1, superv_x
+   
+
+class ConvLSTMDynamic(nn.Module):
+    def __init__(self, num_classes=61, mem_size=512):
+        super(ConvLSTMDynamic, self).__init__()
+        self.num_classes = num_classes
+        self.resNet = resnetMod.resnet34(True, True)
+        self.mem_size = mem_size
+        self.weight_softmax = self.resNet.fc.weight
+        self.lstm_cell = MyConvLSTMCell(512, mem_size)
+        self.avgpool = nn.AvgPool2d(7)
+        self.dropout = nn.Dropout(0.7)
+        self.fc = nn.Linear(mem_size, self.num_classes)
+        self.classifier = nn.Sequential(self.dropout, self.fc)
+        self.dinam = DynamicFilters(512, 3 * 3)
+        self.conv = nn.Sequential(nn.Conv2d(512, 512, kernel_size=1, padding=0),
+                                  nn.ReLU())
+        # self.convDynamic=   nn.Conv2d(3, 3, 3, stride=1,padding=2, bias=False)
+
+    def forward(self, x):
+        state = (torch.zeros((x.size(1), self.mem_size, 7, 7)).cuda(),
+                 torch.zeros((x.size(1), self.mem_size, 7, 7)).cuda())
+        output_t = []
+        for t in range(x.size(0)):
+            output = []
+            logit, feature_conv, feature_convNBN = self.resNet(x[t])
+            
+            
+            
+            bz, nc, h, w = feature_conv.size()
+            feature_conv1 = feature_conv.view(bz, nc, h * w)
+            probs, idxs = logit.sort(1, True)
+            class_idx = idxs[:, 0]
+            cam = torch.bmm(self.weight_softmax[class_idx].unsqueeze(1), feature_conv1)
+            attention_map = F.softmax(cam.squeeze(1), dim=1)
+            attention_map = attention_map.view(attention_map.size(0), 1, 7, 7)
+            attention_feat = feature_convNBN * attention_map.expand_as(feature_conv)
+            
+            
+            state = self.lstm_cell(attention_feat, state)
+            dynamic_filter = self.dinam(feature_convNBN)
+
+            for i in range(x.shape[1]):
+                output.append(F.conv2d((x[t][i].unsqueeze(0)),
+                                       dynamic_filter[i].float(),
+                                       padding=3))
+            output_t.append(torch.stack(output))
+        output_t = torch.stack(output_t)
+
+        feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
+        feats = self.classifier(feats1)
+        return feats, feats1, output_t.squeeze(2)
